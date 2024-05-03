b6086a982f94269d82dabeffd035491d55511a46
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-05-03 10:56:46 +0200

    update matrix, player and state

--- Main.kt
+++ Main.kt
@@ -1,140 +1,44 @@
 package tictactoe
 
-import kotlin.math.abs
-
-enum class State(val result: String) {
-    GAME_NOT_FINISHED("Game not finished"),
-    DRAW("Draw"),
-    X_WINS("X wins"),
-    O_WINS("O wins"),
-    IMPOSSIBLE("Impossible")
-}
-
-enum class Cell(val sign: Char) {
-    X('X'),
-    O('O'),
-    EMPTY('_');
-
-    companion object {
-        fun fromChar(value: Char): Cell {
-            for (x in Cell.values()) {
-                if (x.sign == value) {
-                    return x
-                }
-            }
-            throw IllegalArgumentException(value.toString())
-        }
+class Game {
+    enum class State(val result: String) {
+        GAME_NOT_FINISHED("Game not finished"),
+        DRAW("Draw"),
+        X_WINS("X wins"),
+        O_WINS("O wins"),
     }
-}
 
-class Game(input: String) {
-    val matrix: List<List<Cell>>
-    var state: State
-    val frequencyCounter: Map<Cell, Int>
+    enum class Cell(val sign: Char) {
+        X('X'),
+        O('O'),
+        EMPTY(' ')
+    }
+
+    enum class Player(val cell: Cell, val state: State) {
+        X(Cell.X, State.X_WINS),
+        O(Cell.O, State.O_WINS)
+    }
+
+    private val matrix: List<MutableList<Cell>> = (1..3).map { mutableListOf(Cell.EMPTY, Cell.EMPTY, Cell.EMPTY) }
+    private var player: Player
+    private var state: State
+    private var emptyCount = 9
 
     init {
-        if (input.length != 9) {
-            throw IllegalArgumentException(input)
-        }
-        matrix = toMatrix(input)
-        frequencyCounter = countCells()
-        state = setState()
+        player = Player.X
+        state = State.GAME_NOT_FINISHED
     }
 
-    private fun countCells(): Map<Cell, Int> {
-        val counter = mutableMapOf<Cell, Int>()
-        for (row in matrix) {
-            for (cell in row) {
-                counter[cell] = counter.getOrDefault(cell, 0) + 1
-            }
-        }
-        return counter.toMap()
-    }
-
-    private fun f(coll: List<Cell>, cll: Cell, crrnt: State, result: State): Result<State> =
-        if (coll.any { it != cll }) {
-            Result.success(crrnt)
-        } else if (crrnt == State.GAME_NOT_FINISHED) {
-            Result.success(result)
-        } else {
-            Result.failure(Exception())//State.IMPOSSIBLE
-        }
-
-    fun checkRow(currentState: State): Result<State> {
-        for (row in matrix) {
-            var t = f(row, Cell.X, currentState, State.X_WINS)
-            if (t.isSuccess) {
-                currentState = t.getOrDefault(currentState)
-            } else if (row.all { it == Cell.O }) {
-                if (currentState == State.GAME_NOT_FINISHED) {
-                    currentState = State.O_WINS
-                } else {
-                    return Result.failure(Exception())
-                }
-            }
+    fun updateState() {
+        state = when {
+            matrix.any { it.all { it == player.cell } } -> player.state
+            matrix.indices.any { matrix.map { row -> row[it] }.all { it == player.cell } } -> player.state
+            matrix.indices.all { matrix[it][it] == player.cell } -> player.state
+            matrix.indices.all { matrix[it][matrix.lastIndex - it] == player.cell } -> player.state
+            emptyCount == 0 -> State.DRAW
+            else -> state
         }
     }
-
-    fun setState(): State {
-        var currentState = State.GAME_NOT_FINISHED
-        if (abs(frequencyCounter.getOrDefault(Cell.X, 0) - frequencyCounter.getOrDefault(Cell.O, 0)) > 1) {
-            return State.IMPOSSIBLE
-        }
-
-
-
-        for (i in 0..2) {
-            val column = matrix.map { it[i] }
-            if (column.all { it == Cell.X }) {
-                if (currentState == State.GAME_NOT_FINISHED) {
-                    currentState = State.X_WINS
-                } else {
-                    return State.IMPOSSIBLE
-                }
-            } else if (column.all { it == Cell.O }) {
-                if (currentState == State.GAME_NOT_FINISHED) {
-                    currentState = State.O_WINS
-                } else {
-                    return State.IMPOSSIBLE
-                }
-            }
-        }
-
-        if ((0..2).all { matrix[it][it] == Cell.X }) {
-            if (currentState == State.GAME_NOT_FINISHED) {
-                currentState = State.X_WINS
-            } else {
-                return State.IMPOSSIBLE
-            }
-        } else if ((0..2).all { matrix[it][it] == Cell.O }) {
-            if (currentState == State.GAME_NOT_FINISHED) {
-                currentState = State.O_WINS
-            } else {
-                return State.IMPOSSIBLE
-            }
-        } else if ((0..2).all { matrix[it][2 - it] == Cell.X }) {
-            if (currentState == State.GAME_NOT_FINISHED) {
-                currentState = State.X_WINS
-            } else {
-                return State.IMPOSSIBLE
-            }
-        } else if ((0..2).all { matrix[it][2 - it] == Cell.O }) {
-            if (currentState == State.GAME_NOT_FINISHED) {
-                currentState = State.O_WINS
-            } else {
-                return State.IMPOSSIBLE
-            }
-        } else if (frequencyCounter.getOrDefault(Cell.EMPTY, 0) == 0) {
-            if (currentState == State.GAME_NOT_FINISHED) {
-                currentState = State.DRAW
-            }
-        }
-        return currentState
-    }
-
-    private fun toMatrix(input: String): List<List<Cell>> =
-        input.chunked(3) { it.toList().map { x -> Cell.fromChar(x) } }
-
 
     fun printMatrix() {
         val printBorder = {  println("-".repeat(9)) }
@@ -150,10 +54,43 @@ class Game(input: String) {
     fun printState() {
         println(state.result)
     }
+
+    fun updateMatrix() {
+        while (true) {
+            val line = readln()
+            if (line.contains("[^\\d\\s]".toRegex())) {
+                println("You should enter numbers!")
+            } else if (line.contains("[04-9]".toRegex())) {
+                println("Coordinates should be from 1 to 3!")
+            } else {
+                val (a, b) = line.split(" ").map { x -> x.toInt() }
+                if (matrix[a - 1][b - 1] != Cell.EMPTY) {
+                    println("This cell is occupied! Choose another one!")
+                } else {
+                    matrix[a - 1][b - 1] = player.cell
+                    emptyCount--
+                    break
+                }
+            }
+        }
+    }
+
+    fun updatePlayer() {
+        player = if (player == Player.X) Player.O else Player.X
+    }
+
+    fun loop() {
+        printMatrix()
+        while (state == State.GAME_NOT_FINISHED) {
+            updateMatrix()
+            printMatrix()
+            updateState()
+            updatePlayer()
+        }
+        printState()
+    }
 }
 
 fun main() {
-    val game = Game(readln())
-    game.printMatrix()
-    game.printState()
+    Game().loop()
 }
